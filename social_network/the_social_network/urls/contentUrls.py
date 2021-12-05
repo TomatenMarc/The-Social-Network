416caa89d927ddb850e585f33cb6f389a002d5b3
Andreas Burbach <aburbach@compeon.de>
2021-12-05 15:58:55 +0100

    pagination for statement feed

--- social_network/the_social_network/urls/contentUrls.py
+++ social_network/the_social_network/urls/contentUrls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('statements/get/<int:id>/', ShowStatement.as_view(), name="show_statement"),
     path('statements/with/hashtag/', ShowStatementsWithHashtag.as_view(), name="show_statement_with_hashtag"),
     path('statements/feed/', ShowStatementFeed.as_view(), name="show_statement_feed"),
+    path('statements/feed/pagination', ShowStatementFeedPagination.as_view(), name="show_statement_feed_pagination"),
     path('trending/hashtag/', ShowTrendingHashtag.as_view(), name="show_trending_hashtags"),
 ]
